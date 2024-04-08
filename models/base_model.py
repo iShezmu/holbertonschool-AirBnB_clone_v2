2ea1466abd318c9cdf43d5445918801db815b0b6
iShezmu <arteaga25610@gmail.com>
2024-04-07 20:54:03 -0500

    update: upgrade code line

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
-from sqlalchemy import Column, String, datetime
-from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
 class BaseModel(Base):
     """A base class for all hbnb models"""
-
 
     __abstract__ = True
 
@@ -20,38 +17,28 @@ class BaseModel(Base):
     updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if kwargs:
-            if 'updated_at' in kwargs:
-                kwargs['updated_at'] = datetime.strptime(
-                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            if 'created_at' in kwargs:
-                kwargs['created_at'] = datetime.strptime(
-                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-
-            # Ignore __class__ if it's in kwargs
-            kwargs.pop('__class__', None)
-
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-
-        else:
+        """Instantiates a new model"""
+        if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
-
-        self.save()
+        else:
+            for key, value in kwargs.items():
+                # Convert datetime fields from string to datetime object
+                if key in ['created_at', 'updated_at']:
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        cls_name = self.__class__.__name__
+        return '[{}] ({}) {}'.format(cls_name, self.id, self.__dict__)
 
     def save(self):
         """Saves the object to the database"""
         from models import storage
         self.updated_at = datetime.utcnow()
-        storage.new(self)
         storage.save()
 
     def delete(self):
@@ -61,12 +48,8 @@ class BaseModel(Base):
 
     def to_dict(self):
         """Convert instance into dict format"""
-        new_dict = self.__dict__.copy()
+        new_dict = {key: value for key, value in self.__dict__.items() if key != '_sa_instance_state'}
         new_dict['__class__'] = self.__class__.__name__
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
-
-        # rm _sa_instance_state, if it exits, before returning dict
-        new_dict.pop('_sa_instance_state', None)
-
         return new_dict

037ebec1cec3dfcae93dc9a0c6099850cacf93c8
iShezmu <arteaga25610@gmail.com>
2024-04-07 21:30:25 -0500

    update: update documentation and add or replace classes

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.base_model import Base, BaseModel
 
 class User(BaseModel, Base):
@@ -10,3 +11,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    # Relationship between User and Place
+    places = relationship("Place", back_populates="user", cascade="all, delete, delete-orphan")

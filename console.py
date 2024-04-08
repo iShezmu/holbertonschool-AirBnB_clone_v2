1e79aa598fcb88e11e0a70b78c3da86b6c52d0b3
iShezmu <arteaga25610@gmail.com>
2024-04-07 20:55:37 -0500

    update: modify 'do_create' and 'do_all'

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] =='}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -114,39 +114,31 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """ Create an object of any class with given parameters"""
-        arg_list = arg.split()
-        if len(arg_list) == 0:
+        """Create an object of any class with given parameters"""
+        arg_list = args.split()
+        if not arg_list:
             print("** class name missing **")
             return
         class_name = arg_list[0]
         if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
+
         parameters = {}
         for param in arg_list[1:]:
-            # Split the parameter by the first '=' encountered
             key_value = param.split('=', 1)
             if len(key_value) == 2:
                 key, value = key_value
-                key = key.replace('_', ' ')
                 try:
-                    # String value
-                    if value[0] == "\"" and value[-1] == "\"":
-                        value = value.strip("\"").replace('_', ' ').replace('\"', '"')
-                    # Integer value
-                    elif value.isdigit():
-                        value = int(value)
-                    # Float value
-                    else:
-                        value = float(value)
+                    # Attempt to process and assign the value
                     parameters[key] = value
                 except ValueError:
-                    continue # Skip invalid parameter
+                    continue  # Skip to the next parameter
+        
         # Create a new instance and set attributes
         new_instance = HBNBCommand.classes[class_name]()
         for key, value in parameters.items():
-            setattr(new_instance. key, value)
+            setattr(new_instance, key, value)
         new_instance.save()
         print(new_instance.id)
         
@@ -223,21 +215,18 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, args):
         """ Shows all objects, or all objects of a class"""
-        print_list = []
+        args_list = args.split()
 
         if args:
-            args = args.split(' ')[0]  # remove possible trailing args
-            if args not in HBNBCommand.classes:
+            if args_list [0] not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
-                if k.split('.')[0] == args:
-                    print_list.append(str(v))
+            dict_objs = storage.all(HBNBCommand.classes[args_list[0]]) if args_list else storage.all()
         else:
-            for k, v in storage._FileStorage__objects.items():
-                print_list.append(str(v))
-
-        print(print_list)
+            dict_objs = storage.all()
+            
+        for obj in dict_objs.values():
+            print(obj)
 
     def help_all(self):
         """ Help information for the all command """
@@ -296,7 +285,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -304,10 +293,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
